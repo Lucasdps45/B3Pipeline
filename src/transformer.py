8361ddfc71a0de1c7d044d16ea095aba407e2097
Lucasdps45 <luckinhasdps45@gmail.com>
2026-02-27 18:36:29 -0300

    fix: corrected df KeyError

--- src/transformer.py
+++ src/transformer.py
@@ -4,7 +4,7 @@ import pandas as pd
 def transform_data(df):
     
     df['preco'] = df['preco'].astype(float)
-    df['variacao'] = df['variacao'].astype(float)
+    df['variacao_pct'] = df['variacao_pct'].astype(float)
     df['volume'] = df['volume'].astype(int)
     df['data_coleta'] = pd.to_datetime(df['data_coleta'])
 
